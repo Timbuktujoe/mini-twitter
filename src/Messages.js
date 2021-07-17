2681d32ff5961d0c8666e5afb7dd0779f73cc2e4
Natalia Trzeciak <trzeciak.natalia@icloud.com>
2021-07-17 10:43:08 +0200

    Routing-almost-working

--- src/Messages.js
+++ src/Messages.js
@@ -2,9 +2,9 @@ import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import Spinner from 'react-bootstrap/Spinner'
 import {BiSort} from 'react-icons/bi'
+import Message from "./Message"
 
-
-export default function Messages({userData, setUserData}) {
+export default function Messages({userData, setUserData, component}) {
 
     const handleDate =(b)=>{
         console.log(userData)
@@ -17,22 +17,9 @@ export default function Messages({userData, setUserData}) {
 
         <>
             <p>Sort by date <BiSort className="sorting_icon" onClick={handleDate}/></p>
-            {userData.length  ? userData.map((i) =>
-            
-                <Card key={i.id} border="primary" style={{ margin: "10px" }}>
-                    <Card.Header>@{i.firstName}_{i.lastName}</Card.Header>
-                    <Card.Body>
-                        <Card.Title>Funny Title</Card.Title>
-                        <Card.Text>
-                            Deep, meaningful placeholder message that can fit into 150 characters or something, idk, never used twitter lmao
-                        </Card.Text>
-                        <Button variant="outline-primary">Check it out</Button>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Posted on {i.message_createdAt}</small>
-                    </Card.Footer>
-                </Card>
-                
+            {userData.length  ? userData.map((msg) =>
+               <Message key={msg.id} firstName={msg.firstName} lastName={msg.lastName} id={msg.id} timeStamp={msg.message_createdAt}/>
+               
             ) : <Spinner animation="border" role="status">
                 <span className="visually-hidden">Loading...</span>
             </Spinner>
